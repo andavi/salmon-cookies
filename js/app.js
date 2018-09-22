8b92b57e4fc42488167d3553b994ff04c9e52150
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-09-22 13:05:48 -0700

    set up stores, get cookies per hour and totals

--- js/app.js
+++ js/app.js
@@ -1,1 +1,59 @@
 'use strict';
+
+// stores
+var firstAndPike = {
+  min: 23,
+  max: 65,
+  avgCookies: 6.3,
+  customersPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  }
+};
+var seaTac = {
+  min: 3,
+  max: 24,
+  avgCookies: 1.2,
+  customersPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  }
+};
+var seattleCenter = {
+  min: 11,
+  max: 38,
+  avgCookies: 3.7,
+  customersPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  }
+};
+var capitolHill = {
+  min: 20,
+  max: 38,
+  avgCookies: 2.3,
+  customersPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  }
+};
+var alki = {
+  min: 2,
+  max: 16,
+  avgCookies: 4.6,
+  customersPerHour: function() {
+    return Math.random() * (this.max - this.min) + this.min;
+  }
+};
+
+var stores = [firstAndPike, seaTac, seattleCenter, capitolHill, alki];
+
+// generate cookies per hour arrays and totals
+var generateCookiesPerHour = store => {
+  var cookiesPerHour = [];
+  for (var i = 0; i < 15; i++) {
+    cookiesPerHour.push(Math.floor(store.customersPerHour()));
+  }
+  cookiesPerHour.push(cookiesPerHour.reduce((a, b) => a + b));
+  return cookiesPerHour;
+};
+
+stores.forEach(store => store.cookiesPerHour = generateCookiesPerHour(store));
+
+console.log(firstAndPike);
