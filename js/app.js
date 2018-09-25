541cf22ec81777dd6650af32ccbd25e8424c120b
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-09-25 00:24:17 -0700

    prototype methods in class now

--- js/app.js
+++ js/app.js
@@ -1,16 +1,55 @@
 'use strict';
 
-// store constructor
+// store class
 class Store {
   constructor(location, min, max, avgCookies) {
     this.location = location;
     this.min = min;
     this.max = max;
     this.avgCookies = avgCookies;
+    this.data = document.getElementById('data');
   }
 
-  customersPerHour() {
+  get customersPerHour() {
     return Math.random() * (this.max - this.min) + this.min;
+  }
+
+  generateCookiesPerHour() {
+    this.cookiesPerHour = [];
+    for (let i = 0; i < 15; i++) {
+      this.cookiesPerHour.push(Math.round(this.customersPerHour * this.avgCookies));
+    }
+    this.cookiesPerHour.push(this.cookiesPerHour.reduce((a, b) => a + b));
+  }
+
+  get times() {
+    var times = [];
+    for (let i = 6; i < 12; i++) {
+      times.push(i + 'am: ');
+    }
+    times.push('12pm: ');
+    for (let i = 1; i < 9; i++) {
+      times.push(i + 'pm: ');
+    }
+    times.push('Total: ');
+    return times;
+  }
+
+  renderUL() {
+    var div = document.createElement('div');
+    var h3 = document.createElement('h3');
+    h3.textContent = this.location;
+
+    var ul = document.createElement('ul');
+    this.cookiesPerHour.forEach((n, i) => {
+      var li = document.createElement('li');
+      li.textContent = this.times[i] + n + ' cookies';
+      ul.appendChild(li);
+    });
+
+    div.appendChild(h3);
+    div.appendChild(ul);
+    this.data.appendChild(div);
   }
 }
 
@@ -26,52 +65,8 @@ var stores = [firstAndPike, seaTac, seattleCenter, capitolHill, alki];
 
 
 // generate cookies/hour arrays and totals
-Store.prototype.generateCookiesPerHour = function() {
-  this.cookiesPerHour = [];
-  for (let i = 0; i < 15; i++) {
-    this.cookiesPerHour.push(Math.round(this.customersPerHour() * this.avgCookies));
-  }
-  this.cookiesPerHour.push(this.cookiesPerHour.reduce((a, b) => a + b));
-};
-
 stores.forEach(store => store.generateCookiesPerHour());
 
 
-// generate time strings array
-Store.prototype.generateTimes = () => {
-  var times = [];
-  for (let i = 6; i < 12; i++) {
-    times.push(i + 'am: ');
-  }
-  times.push('12pm: ');
-  for (let i = 1; i < 9; i++) {
-    times.push(i + 'pm: ');
-  }
-  times.push('Total: ');
-  return times;
-};
-
-Store.prototype.times = Store.prototype.generateTimes();
-
-
 // generate unordered list html and append
-Store.prototype.data = document.getElementById('data');
-
-Store.prototype.renderUL = function() {
-  var div = document.createElement('div');
-  var h3 = document.createElement('h3');
-  h3.textContent = this.location;
-
-  var ul = document.createElement('ul');
-  this.cookiesPerHour.forEach((n, i) => {
-    var li = document.createElement('li');
-    li.textContent = this.times[i] + n + ' cookies';
-    ul.appendChild(li);
-  });
-
-  div.appendChild(h3);
-  div.appendChild(ul);
-  this.data.appendChild(div);
-};
-
 stores.forEach(store => store.renderUL());
